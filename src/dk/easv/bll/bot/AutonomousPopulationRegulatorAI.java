3abd69e815422a2c3f67183e7c6ede297ddaac0d
Zeroject <cypeyt@gmail.com>
2022-03-07 10:29:47 +0100

    Create AutonomousPopulationRegulatorAI.java

---
+++ src/dk/easv/bll/bot/AutonomousPopulationRegulatorAI.java
@@ -0,0 +1,17 @@
+package dk.easv.bll.bot;
+
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+
+public class AutonomousPopulationRegulatorAI implements IBot
+{
+    @Override public IMove doMove(IGameState state)
+    {
+        return null;
+    }
+
+    @Override public String getBotName()
+    {
+        return "Autonomous Population Regulator A.I.";
+    }
+}

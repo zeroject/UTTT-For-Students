d44839565d54dbe88175e9dae0f15da613385fe5
Ominousity <abs@hotmail.dk>
2022-03-08 13:22:35 +0100

    yo

--- src/dk/easv/bll/bot/AutonomousPopulationRegulator.java
+++ src/dk/easv/bll/bot/AutonomousPopulationRegulator.java
@@ -3,13 +3,18 @@ package dk.easv.bll.bot;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
-public class AutonomousPopulationRegulatorAI implements IBot
+import java.util.List;
+
+public class AutonomousPopulationRegulator implements IBot
 {
     private static final String BOTNAME="Autonomous Population Regulator A.I.";
 
     @Override public IMove doMove(IGameState state)
     {
-        return null;
+        List<IMove> miniBoardMoves = state.getField().getAvailableMoves();
+
+        return miniBoardMoves.get(0);
+        //return null;
     }
 
     @Override public String getBotName()
